6a699d7279963835bdd8c35bb825b09765a490dd
Rodrigo López Dato <rodrigo@auth0.com>
2014-10-20 14:08:16 -0300

    Update delegation example to Lock, parameterize api_type

--- examples/widget-with-thirdparty-api/app.js
+++ examples/widget-with-thirdparty-api/app.js
@@ -1,27 +1,33 @@
 $(document).ready(function() {
-    var widget = new Auth0Widget({
-        domain: AUTH0_DOMAIN,
-        clientID: AUTH0_CLIENT_ID,
-        callbackURL: AUTH0_CALLBACK_URL,
-        callbackOnLocationHash: true
-    });
+    var lock = new Auth0Lock(
+      AUTH0_CLIENT_ID,
+      AUTH0_DOMAIN
+    );
 
     var userProfile;
 
     $('.btn-login').click(function(e) {
       e.preventDefault();
-      widget.signin({ popup: true} , null, function(err, profile, token) {
+      lock.showSignin(function(err, profile, token) {
         if (err) {
           // Error callback
           console.log("There was an error");
           alert("There was an error logging in");
         } else {
-          // Success calback
-
-          widget.getClient().getDelegationToken('IckaP4QRfGSRGuVZfP9VJBUdlXtgcS4o', token,
+          console.log("Auth0 token", token);
+          // Success callback
+          var delegationOptions = {
+            id_token: token,
+            api: API_TYPE
+          };
+          lock.getClient().getDelegationToken(delegationOptions,
             function(err, thirdPartyApiToken) {
-              localStorage.setItem('thirdPartyApiToken', thirdPartyApiToken.id_token);
-              console.log("Third party token", thirdPartyApiToken.id_token);
+              if (err) {
+                console.log("There was an error getting a delegation token: " + JSON.stringify(err));
+              } else {
+                localStorage.setItem('thirdPartyApiToken', thirdPartyApiToken.id_token);
+                console.log("Third party token", thirdPartyApiToken.id_token);
+              }
             });
 
           // Save the JWT token.
